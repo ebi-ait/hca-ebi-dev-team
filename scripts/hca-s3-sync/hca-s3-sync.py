ad4a917f007bedcea9fceacd33fc580dad74e9da
Rolando Fernandez <rolando@ebi.ac.uk>
2020-01-13 17:15:30 +0000

    added script for copying wrangler data from old HCA account

---
+++ scripts/hca-s3-sync/hca-s3-sync.py
@@ -0,0 +1,29 @@
+from multiprocessing.dummy import Pool
+import sys
+import json
+import subprocess
+
+
+def sync_bucket_dir(source_dir, destination_bucket):
+    print(f'Syncing {source_dir} to bucket {destination_bucket}')
+    cmd = ['aws', 's3', 'sync', source_dir, destination_bucket]
+    p = subprocess.Popen(cmd, shell=True)
+    for line in p.stdout:
+        print(line)
+    p.wait()
+    print(f'Completed syncing {source_dir} to bucket {destination_bucket}')
+    return p.returncode
+
+
+def sync(source_dirs, destination_bucket):
+    with Pool(30) as p:
+        p.map(lambda source_dir: sync_bucket_dir(source_dir, destination_bucket), source_dirs)
+        return 0
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    destination_bucket = sys.argv[2]
+    with open(filename, 'rb') as f:
+        source_dirs = json.load(f)["source_dirs"]
+        sync(source_dirs, destination_bucket)
