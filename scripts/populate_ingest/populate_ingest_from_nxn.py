13ebc8f6ffad848690068958c69bcb4c5c6db40b
Yusra Haider <yhaider@ebi.ac.uk>
2021-09-20 15:11:02 +0100

    ebi-ait/dcp-ingest-central#382: review compliance - used consistent quoting

--- scripts/populate_ingest/populate_ingest_from_nxn.py
+++ scripts/populate_ingest/populate_ingest_from_nxn.py
@@ -74,23 +74,23 @@ class Populate:
 
     def __compare_on_accession__(self):
         ingest_data_accessions = set(itertools.chain.from_iterable(
-            [data.get("insdc_project_accessions") for data in self.ingest_data if
-             data.get("insdc_project_accessions")] +
-            [data.get("geo_series_accessions") for data in self.ingest_data if data.get("geo_series_accessions")] +
-            [data.get("ega_accessions") for data in self.ingest_data if data.get("ega_accessions")] +
-            [data.get("dbgap_accessions") for data in self.ingest_data if data.get("dbgap_accessions")] +
-            [data.get("array_express_accessions") for data in self.ingest_data if
-             data.get("array_express_accessions")] +
-            [data.get("biostudies_accessions") for data in self.ingest_data if data.get("biostudies_accessions")] +
-            [data.get("insdc_study_accessions") for data in self.ingest_data if data.get("insdc_study_accessions")]))
+            [data.get('insdc_project_accessions') for data in self.ingest_data if
+             data.get('insdc_project_accessions')] +
+            [data.get('geo_series_accessions') for data in self.ingest_data if data.get('geo_series_accessions')] +
+            [data.get('ega_accessions') for data in self.ingest_data if data.get('ega_accessions')] +
+            [data.get('dbgap_accessions') for data in self.ingest_data if data.get('dbgap_accessions')] +
+            [data.get('array_express_accessions') for data in self.ingest_data if
+             data.get('array_express_accessions')] +
+            [data.get('biostudies_accessions') for data in self.ingest_data if data.get('biostudies_accessions')] +
+            [data.get('insdc_study_accessions') for data in self.ingest_data if data.get('insdc_study_accessions')]))
 
         filter_accessions = self.nxn_data.get_values('Data location') - ingest_data_accessions
         self.nxn_data.data = [row for row in self.nxn_data.data if self.nxn_data.get_value(row, 'Data location') in filter_accessions
                               or not self.nxn_data.get_value(row, 'Data location')]
 
     def __compare_on_title__(self):
-        ingest_data_pubs = list(itertools.chain.from_iterable([data.get("publications") for data in self.ingest_data if data.get("publications")]))
-        ingest_data_titles = set([reformat_title(pub.get("title")) for pub in ingest_data_pubs if pub.get("title")])
+        ingest_data_pubs = list(itertools.chain.from_iterable([data.get('publications') for data in self.ingest_data if data.get('publications')]))
+        ingest_data_titles = set([reformat_title(pub.get('title')) for pub in ingest_data_pubs if pub.get('title')])
 
         filter_titles = {reformat_title(title) for title in self.nxn_data.get_values('Title')} - ingest_data_titles
         filter_titles = {title for title in filter_titles if not any([get_distance_metric(title, tracking_title)
@@ -104,17 +104,17 @@ class Populate:
 
     def filter(self):
         self.nxn_data.data = [row for row in self.nxn_data.data if
-                         self.nxn_data.get_value(row, 'Organism').lower() in ["human", "human, mouse", "mouse, human"]]
+                         self.nxn_data.get_value(row, 'Organism').lower() in ['human', 'human, mouse', 'mouse, human']]
         self.nxn_data.data = [row for row in self.nxn_data.data if
-                         any([tech.strip() in ["chromium", "drop-seq", "dronc-seq", "smart-seq2", "smarter",
-                                               "smarter (C1)"] for tech in
-                              self.nxn_data.get_value(row, 'Technique').lower().split("&")])]
+                         any([tech.strip() in ['chromium', 'drop-seq', 'dronc-seq', 'smart-seq2', 'smarter',
+                                               'smarter (C1)'] for tech in
+                              self.nxn_data.get_value(row, 'Technique').lower().split('&')])]
         self.nxn_data.data = [row for row in self.nxn_data.data if self.nxn_data.get_value(row, 'Measurement').lower() == 'rna-seq']
 
     def __convert__(self, nxn_data_row) -> dict:
         ingest_project = {
             'content': {
-                "schema_type": "project",
+                'schema_type': 'project',
                 "describedBy": self.ingest_schema
             },
             'cellCount': self.nxn_data.get_value(nxn_data_row, 'Reported cells total'),
@@ -151,15 +151,15 @@ class Populate:
                     added_projects.append(uuid)
                     logging.info(f'added to ingest with uuid {uuid}')
                 except:
-                    logging.exception("")
+                    logging.exception('')
         return added_projects
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Add projects to ingest from Valentine's / NXN database")
-    parser.add_argument("-u", "--url", type=str, help="Base URL for Ingest API", default=INGEST_URL)
-    parser.add_argument("-tp", "--token-path", type=str, help="Text file containing an ingest token", required=True)
-    parser.add_argument("-w", "--write", action='store_true', help="Write to ingest")
+    parser.add_argument('-u', '--url', type=str, help='Base URL for Ingest API', default=INGEST_URL)
+    parser.add_argument('-tp', '--token-path', type=str, help='Text file containing an ingest token', required=True)
+    parser.add_argument('-w', '--write', action='store_true', help='Write to ingest')
 
     args = parser.parse_args()
 
