718d154c94be224141214b78c6156d529203a6a1
Amnon Khen <amnon@ebi.ac.uk>
2021-09-24 12:19:19 +0100

    dcp-382 use assertpy, fix assertion

--- scripts/populate_ingest/tests/test_populate_ingest_from_nxn.py
+++ scripts/populate_ingest/tests/test_populate_ingest_from_nxn.py
@@ -2,15 +2,17 @@ import json
 import unittest
 from unittest.mock import MagicMock, patch
 from populate_ingest import populate_ingest_from_nxn
-
+from assertpy import assert_that
 mock_ingest_api_url = "http://mockingestapi.com"
 mock_token_path = "tests/mock_token.txt"
 
 
+# test data taken from
+# https://docs.google.com/spreadsheets/d/1En7-UV0k0laDiIfjFkdn7dggyR7jIk3WH8QgXaMOZF0/edit#gid=0
 class TestPopulateIngestFromNxn(unittest.TestCase):
         @patch('populate_ingest.populate_ingest_from_nxn.NxnDatabaseService.get_nxn_text')
         @patch('populate_ingest.populate_ingest_from_nxn.QuickIngest')
-        def test(self, mock_ingest_api, mock_nxn_db):
+        def test_add_to_ingest_project_count(self, mock_ingest_api, mock_nxn_db):
                 with open('tests/nxn_db.tsv') as f:
                         mock_nxn_db.return_value = f.read()
                 mock_ingest_api.return_value.new_project = MagicMock(return_value={'uuid': {'uuid': 'created project'}})
@@ -19,7 +21,7 @@ class TestPopulateIngestFromNxn(unittest.TestCase):
                         mock_ingest_api.return_value.get_all = MagicMock(return_value=json.load(f))
                 populate_ingest_from_nxn.main(mock_token_path, True)
 
-                assert mock_ingest_api.new_project.call_count == 5
+                assert_that(mock_ingest_api.return_value.new_project.call_count).is_equal_to(5)
 
 
 if __name__ == '__main__':
