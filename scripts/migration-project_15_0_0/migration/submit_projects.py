af9f358f518389b61482e12e348451d52fcf1c3d
Alegria Aclan <aaclan@ebi.ac.uk>
2021-11-19 14:40:52 +0000

    Modify to search submissions instead of getting from report file

--- scripts/migration-project_15_0_0/migration/submit_projects.py
+++ scripts/migration-project_15_0_0/migration/submit_projects.py
@@ -9,14 +9,19 @@ folder location: https://drive.google.com/drive/u/2/folders/1eAya2x0z-q77gYXUg_M
 import logging
 import os
 from datetime import datetime
+from typing import List
+
+import requests
 
 from migration.submission import Submission
 from migration.util import load_json, load_list, write_json
 
 cwd = os.getcwd()
-REPORT_FILE = cwd + '/_local/PROD_add_project_estimated_cell_count-20210930-184927.json'
 INGEST_API_TOKEN = os.environ['INGEST_API_TOKEN']
 PROJECT_WHITELIST = load_list(cwd + '/migration/dcp1-project-uuids.txt')
+INGEST_API = os.environ['INGEST_API_URL']
+INGEST_API.strip('/')
+DEFAULT_HEADERS = {'Content-type': 'application/json'}
 
 # The submission will not be set back to Valid from Exported/Complete state if:
 # 1. the project is updated and if the project is created first before the submission,
@@ -28,10 +33,23 @@ DO_FORCE_SET_VALID = False
 # After state tracker pod is recreated, this can be set to True to submit and export only metadata via the API
 DO_EXPORT = False
 
+
+def get_project_submissions(project: dict) -> List[dict]:
+    submission_envelopes_url = project['_links']['submissionEnvelopes']['href']
+    r = requests.get(submission_envelopes_url, headers=DEFAULT_HEADERS)
+    result = r.json()
+    submission_envelopes = result['_embedded']['submissionEnvelopes'] if '_embedded' in result else []
+    return submission_envelopes
+
+
+def get_project_by_uuid(project_uuid: str):
+    r = requests.get(f'{INGEST_API}/projects/search/findByUuid?uuid={project_uuid}')
+    r.raise_for_status()
+    return r.json()
+
+
 if __name__ == '__main__':
-    report = load_json(REPORT_FILE)
-    submissions_by_project = report.get('project_submission_envelopes')
-    new_content_by_project = report.get('new_content_by_project')
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     counts = {
         'already_valid': 0,
@@ -47,21 +65,26 @@ if __name__ == '__main__':
                        " from Exported/Complete state to Valid")
         logger.warning("Recreating the pod should initialised the state tracker and sync its state")
 
-    for project_uuid, submissions in submissions_by_project.items():
-        if project_uuid not in PROJECT_WHITELIST:
-            continue
+    for project_uuid in PROJECT_WHITELIST:
+        project = get_project_by_uuid(project_uuid)
+        submissions = get_project_submissions(project)
 
-        patched = new_content_by_project.get(project_uuid).get('patched')
-        if not patched:
-            logger.warning(f'project {project_uuid} was not patched')
-            continue
-
-        counts['projects'] += 1
-        submissions = sorted(submissions, key=lambda s: datetime.strptime(s['createdDate'], '%Y-%m-%dT%H:%M:%S.%fZ'))
+        submissions = sorted(submissions, key=lambda s: datetime.strptime(s['submissionDate'], '%Y-%m-%dT%H:%M:%S.%fZ'))
         first_submission = submissions[0]
-        submission_url = first_submission['submission_url']
+        submission_uuid = first_submission['uuid']['uuid']
+        state = first_submission['submissionState']
+        logger.info(f'project {project_uuid} submission {submission_uuid} state: {state}')
+
+        submission_url = first_submission['_links']['self']['href']
+
         submission = Submission(submission_url, INGEST_API_TOKEN)
         submission_state = submission.get_state().lower()
+
+        graph_validation_state = first_submission['graphValidationState']
+        if graph_validation_state.lower() == 'pending':
+            submission.bypass_graph_validation()
+            logger.info(f'bypassed graph validation!')
+
         if submission_state in ['exported', 'complete']:
             if DO_FORCE_SET_VALID:
                 if submission.is_valid():
@@ -69,20 +92,19 @@ if __name__ == '__main__':
                     try:
                         submission.set_to_valid()
                         logger.warning(f'project {project_uuid} submission {submission_url} has been set to valid')
-                        counts['set_to_valid'] += 1
+
                     except Exception as e:
                         logger.exception(e)
                         logger.warning(f'project {project_uuid} submission {submission_url} failed to be set to valid')
                 else:
-                    counts['do_not_set_to_valid'] += 1
+
                     logger.warning(f'project {project_uuid} submission {submission_url} cannot NOT be set to valid')
             else:
-                counts[submission_state] += 1
+
                 logger.warning(f'project {project_uuid} submission {submission_url} is {submission_state}')
 
         elif submission_state == 'valid':
             logger.warning(f'project {project_uuid} submission {submission_url} is already valid')
-            counts['already_valid'] += 1
 
             if DO_EXPORT:
                 logger.warning(f'submitting')
@@ -94,5 +116,3 @@ if __name__ == '__main__':
                     logger.warning(f'submission {submission_url} was not submitted')
         else:
             logger.warning(f'project {project_uuid} submission {submission_url} is {submission_state}')
-
-    write_json(counts, 'counts.txt')
