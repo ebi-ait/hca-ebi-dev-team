51e21358f6298e060ced49d6d5cdedc3a80f7dfe
Jacob Windsor <me@jcbwndsr.com>
2022-02-01 16:59:32 +0100

    Add script to update ontologies

---
+++ scripts/update_ontology_terms/fix_terra.py
@@ -0,0 +1,45 @@
+import os
+import utils
+import logging
+
+RESULTS_DIR = './results/'
+UPDATED_PROJECTS_FILE = os.path.join(RESULTS_DIR, 'updated_projects.txt')
+DCP1_UUIDS_FILE = './dcp1-project-uuids.txt'
+GCP_BUCKET = ' gs://broad-dsp-monster-hca-prod-ebi-storage/prod/'
+
+# 5. If DCP1 - Remove metadata/sequence_file, /links, /descriptors, /data (anything that references data file)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    with open(DCP1_UUIDS_FILE, 'r') as f
+
+    for uuid in utils.read_lines(UPDATED_PROJECTS_FILE):
+        try:
+            if uuid not in list(utils.read_lines(DCP1_UUIDS_FILE)):
+                logger.info(f'{uuid} not a DCP1 project, doing nothing.')
+                continue
+            
+            logger.info(f'{uuid} is a DCP1 project')
+
+            if not input('Would you like to continue with fixing the terra area for this project? (Y/n)').lower() == 'y':
+                continue
+
+            project = utils.get_project_by_uuid(uuid)
+            submission = utils.get_submission_for_project(project)
+
+            submission_state = submission.get_state().lower()
+
+            if submission_state != 'exported':
+                raise Exception(f'Submission must already be exported. State is {submission_state}')
+
+            utils.run_command(f'gsutil rm -r {GCP_BUCKET}/{uuid}/metadata/sequence_file')
+            utils.run_command(f'gsutil rm -rm {GCP_BUCKET}/{uuid}/metadata/supplementary_file')
+            utils.run_command(f'gsutil rm -rm {GCP_BUCKET}/{uuid}/descriptors')
+            utils.run_command(f'gsutil rm -rm {GCP_BUCKET}/{uuid}/links')
+            utils.run_command(f'gsutil rm -rm {GCP_BUCKET}/{uuid}/data')
+            
+        except Exception as e:
+            logger.error(f'Failed on project {uuid}')
+            logger.error(e)
---
+++ scripts/update_ontology_terms/utils.py
@@ -0,0 +1,93 @@
+import requests
+import json
+import os
+from submission import Submission
+import subprocess
+
+INGEST_API_TOKEN = os.environ['INGEST_API_TOKEN']
+INGEST_API = os.environ.get('INGEST_API_URL', 'https://api.ingest.archive.data.humancellatlas.org')
+INGEST_API.strip('/')
+DEFAULT_HEADERS = {'Content-type': 'application/json'}
+
+def read_lines(file: str):
+    with open(file, 'r') as f:
+        for l in f:
+            yield l
+    
+def get_protocol(protocol_uuid: str):
+    r = requests.get(f'{INGEST_API}/protocols/search/findByUuid?uuid={protocol_uuid}')
+    r.raise_for_status()
+    return r.json()
+
+def get_project_for_protocol(protocol: dict):
+    r = requests.get(protocol['_links']['project']['href'])
+    r.raise_for_status()
+    return r.json()
+
+def get_project_by_uuid(uuid: str):
+    r = requests.get(f'{INGEST_API}/projects/search/findByUuid?uuid={uuid}')
+    r.raise_for_status()
+    return r.json()
+
+def get_project_submissions(project: dict):
+    submission_envelopes_url = project['_links']['submissionEnvelopes']['href']
+    r = requests.get(submission_envelopes_url, headers=DEFAULT_HEADERS)
+    result = r.json()
+    submission_envelopes = result['_embedded']['submissionEnvelopes'] if '_embedded' in result else []
+    return submission_envelopes
+
+def patch(url: str, patch: dict):
+    headers = {'Authorization': INGEST_API_TOKEN, 'Content-type': 'application/json'}
+    r = requests.patch(url, data=json.dumps(patch), headers=headers)
+    r.raise_for_status()
+    return r.json()
+
+def write_list_as_lines(file: str, lines):
+    with open(file, 'w+') as f:
+        f.writelines(list(lines))
+
+def get_new_ontology_for_protocol(protocol):
+    end_bias = protocol['content']['end_bias']
+    ontology_text = protocol['content']['library_construction_method']['text']
+
+    if end_bias == '3 prime tag':
+        if 'v2' in ontology_text:
+            return ('EFO:0009899', '10x 3\' v2')
+        elif 'v3' in ontology_text:
+            return ('EFO:0009922', '10x 3\' v3')
+        else:
+            raise Exception('Ontology is neither v2 or v3')
+    elif end_bias == '5 prime tag':
+        if 'v2' in ontology_text:
+            return ('EFO:0009900', '10x 5\' v2')
+        elif 'v3' in ontology_text:
+            raise Exception('No ontology exists for 10x 5\' v3')
+        else:
+            raise Exception('Ontology is neither v2 or v3')
+    else:
+        raise Exception('End bias not accepted')
+
+def get_submission_for_project(project):
+    submissions = get_project_submissions(project)
+    
+    if(len(submissions) > 1):
+        raise Exception(f'Project {project["uuid"]["uuid"]} has more than one submission')
+    
+    submission_url = submissions[0]['_links']['self']['href']
+
+    return Submission(submission_url, INGEST_API_TOKEN)
+
+def get_submission(uuid):
+    r = requests.get(f'{INGEST_API}/submissionEnvelope/search/findByUuid?uuid={uuid}')
+    r.raise_for_status()
+    sub = r.json()
+
+    return Submission(sub['_links']['self']['href'], INGEST_API_TOKEN)
+
+def run_command(command: str):
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    if error:
+        raise Exception(f'Command failed: {error}')
+    return output
