470ebcd1ce7b82b00e2bb1836b32cfdc2ea9a911
Karoly Erdos <karoly@ebi.ac.uk>
2022-03-04 16:37:47 +0000

    Remove comments from the code

--- scripts/retrigger_file_validation/trigger_file_validation.py
+++ scripts/retrigger_file_validation/trigger_file_validation.py
@@ -15,19 +15,16 @@ if __name__ == '__main__':
         'Content-type': 'application/json',
         'Authorization': f'Bearer {TOKEN}'
     }
-    # 1. Open the list of file resources to be revalidated
     FILES_TO_REVALIDATE_FILENAME = f'{get_submission_id(SUBMISSION_URL)}_files_to_revalidate.json'
     with open(FILES_TO_REVALIDATE_FILENAME, 'r') as infile:
         files_to_revalidate = json.load(infile)
 
-    # 2. Transition state to Invalid temporarily
     for file in files_to_revalidate:
         url = file['_links']['invalid']['href']
         print(f'transition to invalid {url}')
         r = requests.put(url, headers=headers)
         r.raise_for_status()
 
-    # 3. Set validationJob in file metadata document in ingest to null so that it will trigger a new job
     for file in files_to_revalidate:
         url = file['_links']['self']['href']
         print(f'patch validation job of {url}')
@@ -38,7 +35,6 @@ if __name__ == '__main__':
 
     time.sleep(5)
 
-    # 4. Set the state to DRAFT so that events will be sent again to the validator and validator will trigger validation
     for file in files_to_revalidate:
         url = file['_links']['invalid']['href']
         url = url.replace('invalid', 'draft')
