cd6c3a1440621bb188c7deae832bd9624c2d12e9
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2023-02-03 17:21:27 +0000

    dcp-885 fix-ups to script to graph validate

--- scripts/dcp885-Bulk_Spreadsheet_Export/graph_validate.py
+++ scripts/dcp885-Bulk_Spreadsheet_Export/graph_validate.py
@@ -1,30 +1,36 @@
+import os
+import json
 from urllib.parse import urljoin
 
 from exported_projects import ExportedProjects
+
+FILE_NAME = 'graph_validation'
 
 class ValidateAzulProjects(ExportedProjects):
     def __init__(self,  **kwargs):
         super().__init__(**kwargs)
         self.filter_status = "Metadata valid"
         self.valid_projects = {}
-        self.triggered = {}
-        self.errored = {}
+        if os.path.isfile(f'{FILE_NAME}.json'):
+            with open(f'{FILE_NAME}.json') as valid_projects_file:
+                self.valid_projects = json.load(valid_projects_file)
 
     def validate_projects(self):
-        projects = self.get_exported_projects()
-        for project in self.get_valid_projects(projects).values():
-            project["graph_validation"] = "Requested" if self.send_graph_validation(project.get("submission_url")) else "Error"
-            self.save_json('graph_validation', self.valid_projects)
+        if not self.valid_projects:
+            self.valid_projects = self.get_valid_projects(self.get_exported_projects())
+            self.save_json(FILE_NAME, self.valid_projects)
+        for project in self.get_valid_projects(self.valid_projects).values():
+            project[FILE_NAME] = "Requested" if self.send_graph_validation(project.get("submission_url")) else "Error"
+            self.save_json(FILE_NAME, self.valid_projects)
 
     def get_valid_projects(self, projects: dict):
-        self.valid_projects = {}
+        valid_projects = {}
         for uuid, proj in projects.items():
             if proj.get("submission_state") == self.filter_status:
-                self.valid_projects[uuid] = proj
-        self.save_json('graph_validation', self.valid_projects)
-        return self.valid_projects
+                valid_projects[uuid] = proj
+        return valid_projects
 
     def send_graph_validation(self, submission_url: str) -> bool:
-        url = urljoin(submission_url, 'graphValidationRequestedEvent')
-        response = self.api.put(url)
+        url = urljoin(f'{submission_url}/', 'graphValidationRequestedEvent')
+        response = self.api.put(url, json={})
         return response.ok
