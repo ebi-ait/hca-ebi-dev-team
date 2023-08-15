ad484b619066b23e1264c620a24afb2fac5b6998
Amnon Khen <amnon@ebi.ac.uk>
2023-08-15 15:28:45 +0300

    dcp-954 populate hca_bionetwork field

--- scripts/update-bionetworks/app.py
+++ scripts/update-bionetworks/app.py
@@ -25,7 +25,7 @@ class Project:
     content: dict = field(default_factory=lambda: dict())
 
     def with_schema_version(self, version: str):
-        self.content['describedBy'] = f'https://schema.staging.data.humancellatlas.org/type/project/{version}/project'
+        self.content['describedBy'] = f'https://schema.data.humancellatlas.org/type/project/{version}/project'
         return self
 
     def __getitem__(self, item):
@@ -57,28 +57,34 @@ class HCABionetwork:
 def update_bionetwork_for_project(project_uuid: str,
                                   bionetwork: HCABionetwork,
                                   api: IngestApi,
-                                  validate=True):
+                                  validate=True,
+                                  update_op='add'):
     project = api.get_project_by_uuid(project_uuid)
     upgrade_schema_to_17_1_0(project, project_uuid)
-    add_bionetwork(project, bionetwork)
+    add_bionetwork(project, bionetwork, update_op)
     if validate:
         # TODO: enable schema validation for the bionetwork document once https://github.com/HumanCellAtlas/metadata-schema/pull/1526 is done
         json_schema = requests.get(project["content"]["describedBy"]).json()
         validate_against_schema(instance=project["content"], schema=json_schema)
     project_url = api.get_link_from_resource(project, 'self')
     api.patch(project_url, json=project)
-    logging.info(f'project {project_uuid} updated with network {bionetwork}')
+    logging.info(f'project {project_uuid} modified. {update_op} with network {bionetwork}. ')
 
 
-def add_bionetwork(project, bionetwork: HCABionetwork):
-    hca_bionetworks: list = project['content'].setdefault('hca_bionetworks', [])
+def add_bionetwork(project, bionetwork: HCABionetwork, update_op):
     bionetwork_as_dict = dataclasses.asdict(bionetwork)
     bionetwork_as_dict['schema_version'] = '1.0.1'
     new_element = True
-    for x in hca_bionetworks:
-        if x['name'] == bionetwork.name:
-            new_element = False
-            x.update(bionetwork_as_dict)
+    hca_bionetworks: list = project['content'].setdefault('hca_bionetworks', [])
+    if update_op == 'add':
+        for x in hca_bionetworks:
+            if x['name'] == bionetwork.name:
+                new_element = False
+                x.update(bionetwork_as_dict)
+    elif update_op == 'replace':
+        hca_bionetworks.clear()
+    else:
+        raise ValueError(f'update_op {update_op} not supported. Only use "add" or "replace')
     if new_element:
         hca_bionetworks.append(bionetwork_as_dict)
 
@@ -108,16 +114,18 @@ def run():
     api.set_token(f'Bearer {token}')
     # TODO: uuid param from command line
     project_uuids = [
-        '453d7ee2-319f-496c-9862-99d397870b63',
+        'e4b2e4d9-2b9b-46cf-b0e0-bb23ff28030a',
     ]
     for uuid in project_uuids:
         try:
             update_bionetwork_for_project(uuid,
-                                          HCABionetwork(name='Lung',
-                                                        hca_tissue_atlas='Lung',
+                                          HCABionetwork(name='Kidney',
+                                                        hca_tissue_atlas='Kidney',
                                                         hca_tissue_atlas_version='v1.0',
                                                         atlas_project=False),
-                                          api)
+                                          api,
+                                          validate=False,
+                                          update_op='replace')
         except Exception as e:
             logging.warning(f'problem with project {uuid}', exc_info=True)
 
