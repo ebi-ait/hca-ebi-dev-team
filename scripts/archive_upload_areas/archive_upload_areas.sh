bc3ca5d520a4a0117db71e5a04dddc59757d4e64
Amnon Khen <amnon@ebi.ac.uk>
2025-05-22 21:49:10 +0100

    dcp-1055 s3 cost reduction: upload area archive

--- scripts/archive_upload_areas/archive_upload_areas.sh
+++ scripts/archive_upload_areas/archive_upload_areas.sh
@@ -9,12 +9,12 @@ source .env
 : "${INGEST_TOKEN:?Missing INGEST_TOKEN in .env}"
 
 # Configuration
-PROJECT_PAGE_SIZE=500
+PROJECT_PAGE_SIZE=600
 AUTH_HEADER="Authorization: Bearer ${INGEST_TOKEN}"
 TARGET_STORAGE_CLASS=GLACIER
 WRANGLING_STATE=PUBLISHED_IN_DCP
-CUTOFF_UPDATE_DATE=$(date -v -6m "+%Y-%m-%dT%H:%M:%SZ")
-INGEST_KUBE_REPO=../../../ingest-kube-deployment
+CUTOFF_AGE_MONTHS=5m
+CUTOFF_DATE=$(date -v -$CUTOFF_AGE_MONTHS "+%Y-%m-%dT%H:%M:%SZ")
 
 # Functions
 fetch_projects() {
@@ -27,16 +27,16 @@ fetch_projects() {
     --header "Content-Type: application/json" \
     --header "${AUTH_HEADER}" \
     --data "[{\"field\":\"wranglingState\",\"operator\":\"IS\",\"value\":\"${WRANGLING_STATE}\"}]" \
-    | tee output_01_fetch_projects.txt
+    | tee output_01_fetch_projects.json
 }
 
 filter_old_projects() {
-  jq -r --arg date "$CUTOFF_UPDATE_DATE" '
-    ._embedded.projects
-    | map(select(.updateDate < $date))
-    | map(._links.submissionEnvelopes.href)
-    | .[]
-  ' \
+  jq -r \
+    --arg date "$CUTOFF_DATE" '
+      ._embedded.projects
+      | map(select(.dcpVersion < $date))
+      | map({contentLastModified, updateDate, dcpVersion, u: ._links.submissionEnvelopes.href})
+    ' \
   | tee output_02_filter_old_projects.txt
 }
 
@@ -54,16 +54,17 @@ generate_archive_s3_staging_areas_cmd() {
     --storage-class $TARGET_STORAGE_CLASS \
     --metadata-directive COPY \
   | tee output_04_archive_s3_staging_areas.txt
-
 }
 
 # Main script logic
 
-echo "Archiving s3 upload areas of submissions with status $WRANGLING_STATE older than $CUTOFF_UPDATE_DATE"
+echo "Archiving s3 upload areas of submissions with status $WRANGLING_STATE older than $CUTOFF_DATE"
 
 fetch_projects \
-  | filter_old_projects \
-  | fetch_submission_staging_locations \
-  | generate_archive_s3_staging_areas_cmd
+  | filter_old_projects
+#fetch_projects \
+#  | filter_old_projects \
+#  | fetch_submission_staging_locations \
+#  | generate_archive_s3_staging_areas_cmd
 
 echo "Archival of s3 upload areas done complete."
